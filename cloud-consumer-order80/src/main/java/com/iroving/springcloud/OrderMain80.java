e83717e9366c6831ab3ce2eceaa6d88748c42bbf
iroving <962067750@qq.com>
2020-06-10 21:56:07 +0800

    restTemplate的getForEntity方法，原先的eureka其实已经用到了ribbon，我们可以指定ribbon的负载均衡策略，记住不要在componentScan的包下配置规则，不然就会对所有的请求都生效，因此在同级的建一个目录，new 一个bean，然后在主启动类上指明，某个服务用某个策略 openFeign就是封装一个借口，在接口上指定要调用的服务，通过@FeignClient来指定调用某个服务，然后在方法上定义@GetMapping(value = "/payment/getPaymentById/{id}")。具体调用某个url，然后就可以调用了，然后可以在yml上对feign进行超时设置，默认是1s，你可以设置，然后日志的话就需要在yml上配置某个类的具体啥样，还需要写一个config

--- cloud-consumer-order80/src/main/java/com/iroving/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/iroving/springcloud/OrderMain80.java
@@ -1,11 +1,15 @@
 package com.iroving.springcloud;
 
+import com.iroving.ribbon.Myrule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//针对这个服务采取这个规则
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = Myrule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumerconsul-order80/src/main/java/com/iroving/springcloud/controller/OrderConsulController.java
+++ cloud-consumerconsul-order80/src/main/java/com/iroving/springcloud/controller/OrderConsulController.java
@@ -1,6 +1,7 @@
 package com.iroving.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +21,18 @@ public class OrderConsulController {
     public String payment (){
         String result = restTemplate.getForObject(INVOME_URL+"/payment/consul",String.class);
         return result;
+    }
+
+    @GetMapping("/consumer/payment/consul")
+    public String payment2 (){
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(INVOME_URL + "/payment/consul", String.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            String body = forEntity.getBody();
+            return "成功" + body;
+        } else {
+            return "失败";
+        }
+//        return result;
     }
 
 
